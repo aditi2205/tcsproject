0cabb3fc71e94c2cda022891282bb4e40e467b2e
ankita <ankita.sharma102@gmail.com>
2017-07-09 04:00:12 +0530

    Applied MAterialize CSS theme to entire website

--- djangoserver/portal/forms.py
+++ djangoserver/portal/forms.py
@@ -10,13 +10,12 @@ class DetailsForm(forms.ModelForm):
 
 
 
-
-
 class Quiz(forms.ModelForm):
 	class Meta:
          model=Responses
          fields=['q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20','q21']
-         #label=['q1.ques1','ques2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20','q21']
+
+		 #label=['q1.ques1','ques2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20','q21']
          #labels = {
           #  'q1': ('Writer'),
         #}
