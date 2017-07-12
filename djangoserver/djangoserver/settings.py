627d027204bdaea05e8f34bd748752d1d37c7e64
ankita <ankita.sharma102@gmail.com>
2017-07-12 13:50:26 +0530

    fixed local server problem

--- djangoserver/djangoserver/settings.py
+++ djangoserver/djangoserver/settings.py
@@ -155,6 +155,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'portal/static'),
 )
 
 # Simplified static file serving.
